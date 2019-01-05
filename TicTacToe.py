ef6314cf8ca45172ac64fa17d3d322f83c4d28ed
jmsykes <jmsykes15@gmail.com>
2019-01-05 16:18:59 -0500

    Added GameGraphics

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,21 +1,38 @@
-import Game as game
+import pygame
+import GameGraphics
+#
+# while(not game.game_over):
+#     game.print_game_board()
+#     print("Choose a square "
+#           "or press exit to leave the game")
+#     player_input = input("-> ")
+#     if(player_input == "exit" or game.game_over == True):
+#         print("Have a good day!")
+#         break
+#
+#     elif(len(list(player_input.split(" "))) != 2):
+#         print("Please try again. You want it to be "
+#               "n the format 'x y'")
+#     else:
+#         lst = list(player_input.split(" "))
+#         try:
+#             game.choose_square(int(lst[0]),int(lst[1]))
+#         except(ValueError):
+#             print("Choose a number 0 through 2 for x and y")
+#
 
-while(not game.game_over):
-    game.print_game_board()
-    print("Choose a square "
-          "or press exit to leave the game")
-    player_input = input("-> ")
-    if(player_input == "exit" or game.game_over == True):
-        print("Have a good day!")
-        break
+pygame.init()
 
-    elif(len(list(player_input.split(" "))) != 2):
-        print("Please try again. You want it to be "
-              "n the format 'x y'")
-    else:
-        lst = list(player_input.split(" "))
-        try:
-            game.choose_square(int(lst[0]),int(lst[1]))
-        except(ValueError):
-            print("Choose a number 0 through 2 for x and y")
+#this is ttt
+screen = pygame.display.set_mode((300,325))
+board = GameGraphics.drawBoard(screen)
 
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type is pygame.MOUSEBUTTONDOWN:
+            GameGraphics.boardClick(board)
+
+        GameGraphics.showBoard(screen, board)
