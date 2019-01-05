7abb0a8ea53d267900c2a0365bdfbc1666395089
jmsykes <jmsykes15@gmail.com>
2019-01-05 12:50:32 -0500

    Added Visuals

--- Game.py
+++ Game.py
@@ -11,7 +11,6 @@ import pygame
     By: Jason Sykes
 '''
 
-game_player = "X"
 game_over = False
 
 game_board = [[" "] * 3 for _ in range(3)]
@@ -20,30 +19,21 @@ def build_board(ttt):
     background = pygame.Surface(ttt.get_size())
     background = background.convert()
     background.fill((250,250,250))
-    That is all for now,  we will finish this up tomorrow
-    see you then.  Going to leave this error here.
-    If you want to see this code you can find it on my
-    github at:
-    https://github.com/linewareentertainment/TicTacToe
 
-    and remember comment like and subscribe for more content
-        have a great day
+def change_player(game_player):
+    return ("O" if game_player == "X" else "X")
 
-def change_player():
-    global game_player
-    game_player = ("O" if game_player == "X" else "X")
-
-def choose_square(x,y):
+def choose_square(x,y,game_player):
     if(-1 < x < 3 and -1 < y < 3
             and game_board[x][y] == " "):
         game_board[x][y] = game_player
-        is_game_over()
-        change_player()
+        is_game_over(game_player)
+        # change_player(game_player)
     else:
         print("Position Not available\n")
 
-def is_game_over():
-    global game_player, game_board, game_over
+def is_game_over(game_player):
+    global game_board, game_over
 
     if(any(" " in sublist for sublist in game_board)):
         for row in range(0,3):
